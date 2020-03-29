487b20a7454f9a7fb50123d93c30508e2bf9c225
gems <urgems000@gmail.com>
2020-03-29 18:47:39 +0900

    [modify] can select region

--- src/model/jsonprovider.py
+++ src/model/jsonprovider.py
@@ -4,52 +4,54 @@ from urllib import request
 from urllib.error import HTTPError
 
 
-class InvalidPatchError(Exception):
-    def __init__(self, patch: str):
+class InvalidPatchOrRegionError(Exception):
+    def __init__(self, patch: str, region: str):
         """
         This exception caused by wrong patch format string.
         :param patch: patch string e.g. '9.2.4'
+        :param patch: region string e.g ja_JP
         """
-        self.message = "{0} is invalid format".format(patch)
+        self.message = "patch {0} region {1} is invalid format".format(patch, region)
 
 
-class ChampionJsonProvider():
+class ChampionJsonProvider:
     """
     This class provides Champion json.
     Download it if needed.
     """
     JSON_PATH: str = 'json/{0}'
-    D_DRAGON_URL: str = 'http://ddragon.leagueoflegends.com/cdn/{0}/data/ja_JP/champion.json'
+    D_DRAGON_URL: str = 'http://ddragon.leagueoflegends.com/cdn/{0}/data/{1}/champion.json'
 
     recently_accessed_patch: str
     champions_json: str  # cache the most recently accessed
 
-    def __download_json(self, patch: str) -> str:
+    def __download_json(self, patch: str, region: str) -> str:
         """
         download champion json from d-dragon and save it as file
         :param patch: patch string e.g. '9.2.4'
         :return:
         """
-        req = request.Request(self.D_DRAGON_URL.format(patch))
+        req = request.Request(self.D_DRAGON_URL.format(patch, region))
         try:
             res: HTTPResponse = request.urlopen(req)
         except HTTPError:
-            raise InvalidPatchError(patch)
+            raise InvalidPatchOrRegionError(patch, region)
         with res:
             body = res.read()
         return body.decode()
 
-    def provide(self, patch: str) -> str:
+    def provide(self, patch: str, region: str) -> str:
         """
         return json str that has champion information
         :param patch patch string e.g. '9.2.4'
+        :param region region name e.g. ja_JP
         """
-        file_path = self.JSON_PATH.format(patch + '.json')
+        file_path = self.JSON_PATH.format(region + patch + '.json')
         if exists(file_path):
             with open(file_path) as file:
                 return file.read()
         else:
-            json = self.__download_json(patch)
+            json = self.__download_json(patch, region)
             with open(file_path, 'w+') as file:
                 file.write(json)
             return json
--- src/test/test_jsonprovider.py
+++ src/test/test_jsonprovider.py
@@ -1,16 +1,25 @@
 import pytest
 
 from src.model.jsonprovider import ChampionJsonProvider
-from src.model.jsonprovider import InvalidPatchError
+from src.model.jsonprovider import InvalidPatchOrRegionError
 
 
-def test_happypath_with_nocache():
+PATCH: str = '9.3.1'
+REGION: str = 'ja_JP'
+
+
+def test_happypath():
     json_provider = ChampionJsonProvider()
-    champion_json = json_provider.provide('9.3.1')
-    with open('9.3.1.json') as patch_json:
+    champion_json = json_provider.provide(PATCH, REGION)
+    with open('{0}.json'.format(PATCH)) as patch_json:
         assert champion_json == patch_json.read()
 
 
 def test_invalid_patch():
-    with pytest.raises(InvalidPatchError):
-        ChampionJsonProvider().provide('0.0.0')
+    with pytest.raises(InvalidPatchOrRegionError):
+        ChampionJsonProvider().provide('0.0.0', REGION)
+
+
+def test_invalid_region():
+    with pytest.raises(InvalidPatchOrRegionError):
+        ChampionJsonProvider().provide(PATCH, 'NA>EU')
